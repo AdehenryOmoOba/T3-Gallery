c1376e7839d61c1de03f09745ce855a0d67e15b4
adehenry <adefemiadelaja@gmail.com>
2024-04-16 13:14:07 +0100

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import images from "./images"
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 
 
 const imgUrls = images.map((imageUrl, id) => ({id: id + 1, imageUrl}))
